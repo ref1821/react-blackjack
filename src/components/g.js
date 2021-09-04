e42ff4f6e3c667eed9c56f53d5d8ded7d62be6e5
ref1821 <ref1821@gmail.com>
2021-09-04 16:53:28 -0500

    4/9 -1

---
+++ src/components/g.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './g.css'
+
+export class G extends Component {
+    render() {
+        return (
+            <div className="g">
+                <h3>Our games</h3>
+                <h3>Games in development</h3>
+                <a href="#" className="l">UNO</a>
+            </div>
+        )
+    }
+}
+
+export default G;
--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './nav'
 import Bottom from './bottom'
+import G from './g'
 
 const Games = () => {
     return (
         <div>
             <Nav></Nav>
+            <G></G>
             <Bottom></Bottom>
         </div>
     )
